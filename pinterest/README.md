2d5a3c000d54ecc4872d5848dc486d8f6f3d58db
rebeccabosse <rebeccabosse@hotmail.fr>
2018-10-24 22:35:26 +0200

    ajout des fichiers

---
+++ pinterest/README.md
@@ -0,0 +1,23 @@
+# The Hacking Pinterest
+
+Tu veux faire de la concurrence à Pinterest, donc tu voudrais créer un un site où les utilisateurs peuvent créer des "pins". Chauqe pin contient une URL d'une image sur le net. Les utilisateurs peuvent commenter les pins, mais ne peuvent pas commenter les commentaires.
+
+# Modéles:
+
+*	pin
+*	user
+*	comment
+*	application_record
+
+
+##  Auteurs
+
+# Team Caen
+
+Maxime Beaufils, Bertrand Pelletier, Iliès El Ouartassi
+
+### En savoir plus
+
+Project for The hacking Project
+
+https://www.thehackingproject.org/
---
+++ pinterest/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+10.times do
+  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email)
+end
+10.times do
+  pin = Pin.create(user_id: rand(0..9), title: Faker::DcComics.hero, URL: Faker::Avatar.image)
+end
+10.times do
+  comment = Comment.create(user_id: rand(0..9), pin_id: rand(0..9), content: Faker::Artist.name)
+end
